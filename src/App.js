74bef2f63b00cd943e587f0ff5b2403e668a3f95
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-09 16:22:29 -0300

    connect with backend-gostack11 and create page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
 import "./App.css";
+
+import Header from "./components/Header";
 
 function App() {
   const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}` ]);
+
+    const response = await api.post("/projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Lucas Mendonça",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
     <>
-      <Header title="Homepage" />
+      <Header title="Projects" />
+
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
+
       <button type="button" onClick={handleAddProject}>
         Adicionar projeto
       </button>
