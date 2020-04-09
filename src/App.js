479f2ad0c6afc25bdacf598ea3fc6bb2ec055a16
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-09 15:50:31 -0300

    chore: state and immutability

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Lucas">
-        <ul>
-          <li>Homepage</li>
-        </ul>
-      </Header>
-      <Header title="Mendonça">
-        <ul>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Homepage" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
